4687a345702e6e7cbe14d3a440f86f039157c408
stephan georgi <amende6>
2020-07-23 16:56:33 +0200

    Initial Commit

---
+++ AudiokitSequencer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudiokitSequencer
+//
+//  Created by Georgi, Stephan on 23.07.20.
+//  Copyright © 2020 Georgi, Stephan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
