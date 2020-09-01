8d750c8b286fdc792ce40ef04f8deae66382589e
stephan georgi <amende6>
2020-09-01 11:47:37 +0200

    Add color animation extension

---
+++ AudiokitSequencer/UI/AudioSequencer+UIGradientLayer.swift
@@ -0,0 +1,32 @@
+//
+//  AudioSequencer+UIGradientLayer.swift
+//  AudiokitSequencer
+//
+//  Created by Georgi, Stephan on 31.08.20.
+//  Copyright © 2020 Georgi, Stephan. All rights reserved.
+//
+
+import UIKit
+
+extension CAGradientLayer {
+
+    func setColors(_ newColors: [CGColor],
+                   animated: Bool = true,
+                   withDuration duration: TimeInterval = 0,
+                   timingFunctionName name: CAMediaTimingFunctionName? = nil) {
+
+        if !animated {
+            self.colors = newColors
+            return
+        }
+
+        let colorAnimation = CABasicAnimation(keyPath: "colors")
+        colorAnimation.fromValue = colors
+        colorAnimation.toValue = newColors
+        colorAnimation.duration = duration
+        colorAnimation.isRemovedOnCompletion = false
+        colorAnimation.fillMode = CAMediaTimingFillMode.forwards
+        colorAnimation.timingFunction = CAMediaTimingFunction(name: name ?? .linear)
+        add(colorAnimation, forKey: "colorsChangeAnimation")
+    }
+}
