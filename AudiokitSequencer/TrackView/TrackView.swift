64558ebc75757aef163c907fa9cc778928fd8ab9
stephan georgi <amende6>
2020-09-01 18:01:57 +0200

    Refactor to own groups and files

---
+++ AudiokitSequencer/TrackView/TrackView.swift
@@ -0,0 +1,35 @@
+//
+//  TrackView.swift
+//  AudiokitSequencer
+//
+//  Created by Georgi, Stephan on 01.09.20.
+//  Copyright © 2020 Georgi, Stephan. All rights reserved.
+//
+
+import UIKit
+import AudioKit
+
+class TrackView {
+    let viewModel: TrackViewModel
+    let stackView = UIStackView()
+    var buttons: [PadButton] = [] {
+        didSet {
+            for button in stackView.arrangedSubviews {
+                stackView.removeArrangedSubview(button)
+            }
+            for button in buttons {
+                stackView.addArrangedSubview(button)
+            }
+        }
+    }
+
+    init(trackViewModel: TrackViewModel) {
+        viewModel = trackViewModel
+        stackView.axis = .horizontal
+        stackView.spacing = PadButtonFactory.spacing
+    }
+
+    func updateButtons() {
+        buttons = viewModel.buttons
+    }
+}
